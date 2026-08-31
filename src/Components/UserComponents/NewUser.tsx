901a2de8de05c7405279964a4101c6d68cffd3c6
Matthew Ciccone <mcicconec@yorksolutions.net>
2026-08-31 10:22:51 -0700

    Create user working but not fully functional

---
+++ src/Components/UserComponents/NewUser.tsx
@@ -0,0 +1,63 @@
+import {} from "../../data/types";
+import  { useState } from 'react'
+
+export function NewUser () {
+
+
+
+    const [formData, setFormData] = useState({name: '', email: '', password: ''})
+
+    const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) =>  {
+        setFormData({
+            ...formData, 
+            [event.target.name]: event.target.value
+            }
+         )
+    }
+
+    const handleNewUser = async (event: React.SubmitEvent<HTMLFormElement>): Promise<void> => {
+        event.preventDefault()
+        try{
+            const response = await fetch(`http://localhost:3001/users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+            if (!response.ok) {
+                throw new Error('cannot respond to json file')
+            }
+
+        } catch (error) {
+            console.error(error)
+
+        }
+
+    }
+
+
+
+
+
+
+    
+
+    return (
+        <>
+        <div>
+            <form onSubmit={handleNewUser}>
+                <label htmlFor="name">Name: </label>
+                <input name="name" value={formData.name} onChange={handleFormChange}></input>
+                <label htmlFor="email">Email: </label>
+                <input name='email' value={formData.email} onChange={handleFormChange}></input>
+                <label htmlFor="password">New Password: </label>
+                <input name='password' value={formData.password} onChange={handleFormChange}></input>
+                <button type="submit">New User</button>
+            </form>
+        </div>
+        
+        
+        </>
+    )
+}
---
+++ src/data/types.ts
@@ -0,0 +1,56 @@
+export interface User  {
+    id: string
+    name: string
+    email: string
+    password: string
+    notes: Note[]    
+}
+
+export interface Note {
+    id: string
+    title: string
+    content: string
+    creationDate: Date
+    tags?: string[] 
+}
+
+export type NewUserForm = {
+    name: string
+    email: string
+    password: string
+}
+
+
+
+// using Classes to keep the data types
+
+// class User  {   
+//         id: string;
+//         name: string;
+//         email: string;
+//         password: string;
+//         notes: Note[]
+//         constructor(id: string, name: string, email: string, password: string, notes: Note[]) {
+//             this.id = id;
+//             this.name = name
+//             this.email = email
+//             this.password = password
+//             this.notes = notes
+//         }    
+//     }
+
+// class Note {
+//     id: string
+//     title: string
+//     content: string
+//     creationDate: Date
+//     tags?: string[]
+//         constructor(id: string, title: string, content: string, creationDate: Date, tags?: string[]) {
+//             this.id = id
+//             this.title = title
+//             this.content = content
+//             this.creationDate = creationDate
+//             this.tags = tags // come back to this and see if we need a conditional
+//         }
+// }
+
